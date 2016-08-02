ddfbebd219203bdba5aa4b0c5d7fcc2e74d0fef7
hzmawei <hzmawei@corp.netease.com>
2016-08-02 20:24:56 +0800

    @add misc

--- dapenti/build/lib/dapenti/spiders/spider.py
+++ dapenti/build/lib/dapenti/spiders/spider.py
@@ -6,7 +6,6 @@ from dapenti.items import DapentiItem
 from scrapy import Request
 
 class DPTSpider(scrapy.Spider):
-	def __init__(self):self.download_delay = 0.8
 	name = "dapenti"
 	allowed_domains = ["dapenti.com"]
 	start_urls = ["http://www.dapenti.com/blog/index.asp"]
@@ -31,13 +30,13 @@ class DPTSpider(scrapy.Spider):
 
 	def parse_item(self,response):
 		item = DapentiItem()
-		if(response.status != 200 ):
-			print "Fail to request : ",response.url
-			return None
-		print "dep2 " + response.url
+		print "dep2 " + response.url + str(response.status)
 		item['title'] = response.xpath('/html/body/table/tbody/tr/td[1]/div/table[1]/tbody/tr[1]/td/div/span/span/a[2]/text()').extract_first()
-		item['time'] = response.css('body > table > tbody > tr > td.oblog_t_2 > div > table.ke-zeroborder > tbody > tr:nth-child(2) > td > table:nth-child(1) > tbody > tr > td > div > span::text').extract_first()[-17:]
+		# item['time'] = response.css('body > table > tbody > tr > td.oblog_t_2 > div > table.ke-zeroborder > tbody > tr:nth-child(2) > td > table:nth-child(1) > tbody > tr > td > div > span::text').extract_first()[-17:]
 		# item['participator'] = response.xpath('//*[@id="SOHU_MAIN"]/div[1]/div[1]/div[1]/div/a/span/span/em/text()').extract_first()
+		if item['title'] is None:
+			return None
+		item['time'] = response.xpath('/html/body/table/tbody/tr/td[1]/div/table[1]/tbody/tr[2]/td/table[1]/tbody/tr/td/div/span/text()').extract_first()
 		item['url'] = response.url
 		try:
 			print repr(item).decode("unicode-escape") + '\n'
